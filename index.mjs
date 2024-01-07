3f348004170df7510cf1b7156d55c83b99ee4d67
phhu <phhu.git@gmail.com>
2024-01-07 12:33:39 +0000

    +css

--- index.mjs
+++ index.mjs
@@ -1,33 +1,53 @@
 import express from 'express'
 import 'dotenv/config'
 
-const app = express()
+let message = process.env?.["DEFAULT_MESSAGE"] || "I like dancing"
+const password = process.env?.["PASSWORD"] || ""
 const port = process.env?.["PORT"] || 3000
+
+const app = express()
+app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-let message = process.env?.["DEFAULT_MESSAGE"] || "I like dancing"
-const password = process.env?.["PASSWORD"]
+
+const page = content => `<!DOCTYPE html>
+
+<html>
+<head>
+<link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+
+${content}
+
+<body>
+</head>
+`
 
 app.get('/set', (req,res) => {
-  res.send(`
-    <form method="post" >
+  res.send(page(`    
+      <form method="post" >
       <div><input size="40" name="text" value="${message}" /></div>
       <div><input type="password" name="pwd" /></div>
       <div><input type="submit" value="set" /></div>
     </form>
-  `)
+  `))
 })
 
 app.post('/set', (req,res) => {
   if (req?.body?.pwd === password){
     message = req?.body?.text
   }
-  res.send(`${message} <br /><a href="set">set</a> <a href=".">view</a>`)
+  res.send(`<div class="controls">
+      <a href="set">set</a> 
+      <a href=".">view</a>
+    </div>
+    <div class="message">${message}</div>`)
 })
 
 app.get('*', (req, res) => {
-  res.send(`<br /> <br /><center><h1>${message}</h1></center>`)
+  res.send(page(`<div class="message">${message}</div>`))
 })
 
 app.listen(port, () => {
